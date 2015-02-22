92183723965b20d1fe59c8d7779eb5979da488a8
zackhanni <zack.hanni@gmail.com>
2015-02-22 15:40:36 -0500

    15/17 completed

--- francis.rb
+++ francis.rb
@@ -1,10 +1,11 @@
 class Francis
 	def yo(message)
-		if message.end_with?("?")
+		message.strip!
+		if message.end_with?("?") && message != message.upcase
 			"Sure."
 		elsif message.empty?
 			"See if I care!"
-		elsif message == message.upcase
+		elsif message == message.upcase #&& message.end_with?("!")
 			"Chill!"
 		else
 			"Whatevs."	
