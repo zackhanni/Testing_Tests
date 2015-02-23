481fd0579e00b92dea409c8429dd93fe9e0ed4aa
zackhanni <zack.hanni@gmail.com>
2015-02-22 23:44:41 -0500

    17/17 passed

--- francis.rb
+++ francis.rb
@@ -1,11 +1,11 @@
 class Francis
 	def yo(message)
 		message.strip!
-		if message.end_with?("?") && message != message.upcase
+		if (message.end_with?("?") && message != message.upcase) || (message.end_with?("?") && message.each_char.any? { |character| ("1".."9").cover?(character.downcase) })
 			"Sure."
 		elsif message.empty?
 			"See if I care!"
-		elsif message == message.upcase #&& message.end_with?("!")
+		elsif message == message.upcase && message.each_char.any? { |character| ("a".."z").cover?(character.downcase) }
 			"Chill!"
 		else
 			"Whatevs."	
